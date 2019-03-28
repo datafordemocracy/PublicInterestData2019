89b81936145c86892439d6a6a27c350afd89102c
Michele Claibourn <mclaibourn@virginia.edu>
2019-03-28 09:24:29 -0400

    add referral_long.R

---
+++ code/referral_long.R
@@ -0,0 +1,64 @@
+# Public Interest Data Lab
+# March 26, 2019
+# Work with full referral data; make long
+
+# use full referral
+library(tidyverse)
+setwd("/Volumes/PIDL19")
+
+ref <- read_csv("referral.csv")
+# referral.csv created in readfile.R
+
+# select just cps data
+# SafetyDecision.1 through SafetyDecision.5 missing from referral.csv
+# read in initial matched data and add these 5 columns
+dss <- read_csv("match_cville_CPS_to_FC.csv")
+# match_cville_CPS_to_FC.csv created in readfile.R
+
+dss_safety <- dss %>% 
+  select(CounterID, SafetyDecision.1:SafetyDecision.5)
+
+ref <- left_join(ref, dss_safety) # join
+ref <- ref %>% select(CounterID, FIPS, DOB, Gender, Race, Hispanic, # reorder
+                      Race_Ethnicity, RaceShort_Victim:OverallDisposition.26, 
+                      SafetyDecision.1:SafetyDecision.5, 
+                      SafetyDecision.6:PriorReferralExistsWithinYear.26)
+
+# reshape to long
+reflong <- ref %>% 
+  gather(Region.1:PriorReferralExistsWithinYear.26, key = key, value = value) %>% 
+  separate(key, sep = "\\.", into = c("var", "num")) %>% 
+  split(.$var) %>% 
+  map(~mutate(., !!.$var[1] := value)) %>%  map(~select(., -var, -value)) %>%
+  Reduce(f = merge, x = .) %>%
+  mutate(num = as.numeric(num)) %>% 
+  arrange(CounterID, num) 
+
+# filter out empty rows and reorder variables (to original sequence)
+reflong <- reflong %>% 
+  filter(!is.na(Region) & !is.na(Locality)) %>% 
+  select(CounterID:num, Region, Locality, REFER_ID, ReferralYear, REFER_DT, 
+         Refer_Month_Year, CAS_ID, TrackAssignment, ResponsePriority, 
+         OverallDisposition, SafetyDecision, SCRN_OUT_REFER_SW, ACPT_REFER_SW, 
+         VictimAgeAtReferral, VicAgeTrunc, InvalidAN, MedicalNeglect, 
+         MentalAbuseNeglect, PhysicalAbuse, PhysicalNeglect, SexualAbuse, 
+         SubstanceExposedInfants, AbuserClientID, AbuserBirthDate, 
+         AbuserAgeAtReferral, AbuserGender, AbuserPrimaryEthnicity, 
+         FirstContactDateTime, ResponseTimedays, ResponseTimeTimely, 
+         FirstVictimContactDateTime, VictimResponseTimedays, 
+         VictimResponseTimeTimely, PriorReferralExistsWithinYear)
+
+# check if number of records per child equals NbrRefs
+reflong <- reflong %>% 
+  group_by(CounterID) %>% 
+  mutate(numref = n()) %>% 
+  ungroup()
+
+sum(reflong$numref == reflong$NbrRefs) # yes
+
+# save file for cleaning and analysis
+saveRDS(reflong, file = "ref_long.rds")
+# readRDS("ref_long.rds") # to load
+
+# adapted from stackoverflow example
+# https://stackoverflow.com/questions/53146553/r-gather-multiple-columns-in-multiple-key-columns-with-tidyr
